1d5610d314ed592dbe4f6afeadb53485d5ac230a
qassem.aburas@outlook.com <qassem.aburas@outlook.com>
2019-02-05 16:36:58 +0100

    fixed Angle && velocity

--- 2.GOLF/Program.cs
+++ 2.GOLF/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using _2.GOLF;
+using System;
 using System.Collections.Generic;
 
 namespace Golf2
@@ -6,80 +7,121 @@ namespace Golf2
     class Program
     {
         static double swing = 0;
-        static double maxSwings = 4;
+        static double maxSwings = 5;
         static double ballLocation = 0;
         static double goalLocation = 640;
         static double distanceBetween = 0;
-        static double angle = 0;
-        static double velocity = 0;
+        //static double angle = 0;
+        //static double velocity = 0;
+        static List<Swing> swings = new List<Swing>();
+
+
+
         static void Main(string[] args)
         {
-            bool stayAlive = true;
-            SwingLoop(stayAlive);
 
-        }
-        static void SwingLoop(bool stayAlive)
-        {
             Console.WriteLine("  Welcome to your game GOLF\n\nPress any key to get started");
             Console.ReadKey(true);
-            while (stayAlive)
+
+            do
             {
-                try
+                double angle = AngleM();
+                double velocity = VelocityM();
+                double distance = CalculateDistance(angle, velocity);// var ersätter tydigare typen (double)
+
+                setNewLocation(distance);
+                swing = swing + 1;
+                distanceBetween = Math.Abs(goalLocation - ballLocation);
+                Console.WriteLine("ballLocation:" + ballLocation);
+                Console.WriteLine(distanceBetween + " between the cup and the ball");
+                Console.WriteLine(maxSwings - swing + " swings left");
+
+                Swing SW = new Swing(angle, velocity, distance);
+                swings.Add(SW);
+
+                if (ballLocation == goalLocation)
                 {
-
-                    Console.WriteLine("Enter an angel :");
-                    angle = Convert.ToDouble(Console.ReadLine());
-                    Console.WriteLine("Enter velocity :");
-                    velocity = Convert.ToDouble(Console.ReadLine());
-                    var Distance = CalculateDistance(angle, velocity);// var ersätter tydigare typen (double)
-                    setNewLocation(Distance);
-                    Console.WriteLine(Distance);
-                    swing = swing + 1; // reduce your try(swings)
-
-                    distanceBetween = Math.Abs(goalLocation - ballLocation);
-
-                    Console.WriteLine("ballLocation:" + ballLocation);
-                    Console.WriteLine(distanceBetween + " between the cup and the ball");
-                    Console.WriteLine(maxSwings - swing + " swings left");
-                    if (ballLocation == goalLocation)
-                    {
-                        Console.WriteLine(" You win \nPress any key to close the game"); // ballLocation = 0
-                        Console.ReadKey();
-                        System.Environment.Exit(0);
-                    }
-                    else if (ballLocation >= goalLocation)//the is after the goal > 640
-                    {
-                        Console.WriteLine("You'r atfer the line of the cup, Game Over, try later\n Press any key to end");//max 640, more = lose
-                        Console.ReadKey(true);
-                        System.Environment.Exit(0);
-                    }
-                    else if (swing > maxSwings)
-                    {
-                        Console.WriteLine(" You lose! bad luck, no more swings left\nPress any key to close "); // 0 swings  game over
-                        Console.ReadKey(true);
-                        System.Environment.Exit(0);
-                    }
-
+                    Console.WriteLine(" You win \nPress any key to show your score and close the game"); // ballLocation = 0
+                    Console.ReadKey();
+                    break;
                 }
-                catch
+                else if (distanceBetween > goalLocation)//the is after the goal > 640
                 {
-
+                    Console.WriteLine("You'r atfer the line of the cup, Game Over, try later\n Press any key to end");//max 640, more = lose
+                    Console.ReadKey(true);
+                    break;
                 }
-
-            }
+                else if (swing >= maxSwings)
+                {
+                    Console.WriteLine(" You lose! bad luck, no more swings left\nPress any key to close "); // 0 swings  game over
+                    Console.ReadKey(true);
+                    break;
+                }
+            } while (true);
+            Console.Clear();
+            foreach (var swing in swings)
+            {
+               
+                Console.WriteLine("The historic ");
+                Console.WriteLine("The angle: " + swing.Angle );
+                Console.WriteLine( "The velocity: " + swing.Velocity);
+                Console.WriteLine("The distance: " + swing.Distance); 
+                
+            } Console.ReadKey(true);
         }
         static double CalculateDistance(double angle, double velocity)
         {
             double GRAVITY = 9.8;
 
-            return Math.Pow(velocity, 2) / GRAVITY * Math.Sin(2 * ((Math.PI / 180) * angle));
+            return Math.Round(Math.Pow(velocity, 2) / GRAVITY * Math.Sin(2 * ((Math.PI / 180) * angle)));
 
         }
 
         static void setNewLocation(double distance)
         {
-            ballLocation = ballLocation + distance;
+            if (ballLocation > goalLocation)
+                ballLocation = ballLocation - distance;
+            else
+                ballLocation = ballLocation + distance;
+        }
 
+        static double AngleM()
+        {
+            Console.WriteLine("Enter an angel :");
+            double angle = Convert.ToDouble(Console.ReadLine());
+
+            while (angle < 0.1 || angle > 89) //&& angle < 0 || angle > 90)
+            {
+                try
+                {
+                    Console.WriteLine("Angle should be between 0,1 and 89");
+                    angle = Convert.ToDouble(Console.ReadLine());
+                }
+                catch
+                {
+
+                }
+            }
+            return angle;
+        }
+        static double VelocityM()
+        {
+            Console.WriteLine("Enter velocity :");
+            double velocity = Convert.ToDouble(Console.ReadLine());
+
+            while (velocity < 0.1 || velocity > 109)
+            {
+                try
+                {
+                    Console.WriteLine("Velocity should be between 0,1 and 110");
+                    velocity = Convert.ToDouble(Console.ReadLine());
+                }
+                catch
+                {
+
+                }
+            }
+            return velocity;
         }
     }
 }
---
+++ 2.GOLF/Swing.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _2.GOLF
+{
+    class Swing
+    {
+        public double Angle;
+        public double Velocity;
+        public double Distance;
+
+        public Swing(double angle, double velocity, double distance)
+        {
+            this.Angle = angle;
+            this.Velocity = velocity;
+            this.Distance = distance;
+        }
+    }
+}
