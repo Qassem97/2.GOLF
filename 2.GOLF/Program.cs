523793e7e72d1832c8eed5a84f0ed25b82dc7b93
qassem.aburas@outlook.com <qassem.aburas@outlook.com>
2019-02-06 12:40:23 +0100

    Crash fixed

--- 2.GOLF/Program.cs
+++ 2.GOLF/Program.cs
@@ -27,8 +27,7 @@ namespace Golf2
             {
                 double angle = AngleM();
                 double velocity = VelocityM();
-                double distance = CalculateDistance(angle, velocity);// var ersätter tydigare typen (double)
-
+                double distance = CalculateDistance(angle, velocity);
                 setNewLocation(distance);
                 swing = swing + 1;
                 distanceBetween = Math.Abs(goalLocation - ballLocation);
@@ -41,19 +40,19 @@ namespace Golf2
 
                 if (ballLocation == goalLocation)
                 {
-                    Console.WriteLine(" You win \nPress any key to show your score and close the game"); // ballLocation = 0
+                    Console.WriteLine(" You win \nPress any key to show your score and double press to close the game"); // ballLocation = 0
                     Console.ReadKey();
                     break;
                 }
                 else if (distanceBetween > goalLocation)//the is after the goal > 640
                 {
-                    Console.WriteLine("You'r atfer the line of the cup, Game Over, try later\n Press any key to end");//max 640, more = lose
+                    Console.WriteLine("You'r atfer the line of the cup, Game Over, try later\nPress any key to show your score and double press to close the game");//max 640, more = lose
                     Console.ReadKey(true);
                     break;
                 }
                 else if (swing >= maxSwings)
                 {
-                    Console.WriteLine(" You lose! bad luck, no more swings left\nPress any key to close "); // 0 swings  game over
+                    Console.WriteLine(" You lose! bad luck, no more swings left\nPress any key to show your score and double press to close the game "); // 0 swings  game over
                     Console.ReadKey(true);
                     break;
                 }
@@ -61,13 +60,14 @@ namespace Golf2
             Console.Clear();
             foreach (var swing in swings)
             {
-               
+
                 Console.WriteLine("The historic ");
-                Console.WriteLine("The angle: " + swing.Angle );
-                Console.WriteLine( "The velocity: " + swing.Velocity);
-                Console.WriteLine("The distance: " + swing.Distance); 
-                
-            } Console.ReadKey(true);
+                Console.WriteLine("The angle: " + swing.Angle);
+                Console.WriteLine("The velocity: " + swing.Velocity);
+                Console.WriteLine("The distance: " + swing.Distance);
+
+            }
+            Console.ReadKey(true);
         }
         static double CalculateDistance(double angle, double velocity)
         {
@@ -87,38 +87,38 @@ namespace Golf2
 
         static double AngleM()
         {
-            Console.WriteLine("Enter an angel :");
-            double angle = Convert.ToDouble(Console.ReadLine());
-
-            while (angle < 0.1 || angle > 89) //&& angle < 0 || angle > 90)
+            double angle = 0;
+            Console.WriteLine("Angle should be between 0,1 and 89");
+            while (angle < 0.1 || angle > 89) 
             {
                 try
                 {
-                    Console.WriteLine("Angle should be between 0,1 and 89");
-                    angle = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine("Enter an angel :");
+                    angle = Convert.ToDouble(Console.ReadLine());                
                 }
                 catch
                 {
-
+                    Console.WriteLine("Invalid input! Only numbers are valid.\nPlease try again!");
                 }
             }
             return angle;
+
         }
         static double VelocityM()
         {
-            Console.WriteLine("Enter velocity :");
-            double velocity = Convert.ToDouble(Console.ReadLine());
+            double velocity = 0;
+            Console.WriteLine("Velocity should be between 0,1 and 110");
 
             while (velocity < 0.1 || velocity > 109)
             {
                 try
                 {
-                    Console.WriteLine("Velocity should be between 0,1 and 110");
+                    Console.WriteLine("Enter velocity :");
                     velocity = Convert.ToDouble(Console.ReadLine());
                 }
                 catch
                 {
-
+                   Console.WriteLine("Invalid input! Only numbers are valid.\nPlease try again!");
                 }
             }
             return velocity;
